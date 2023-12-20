2e6d38785de03e704f9b58b942423ae902e58d6d
Olga <gnyh@mail.ru>
2023-12-21 00:13:41 +0300

    доделана лаба3

---
+++ logic/services.py
@@ -0,0 +1,137 @@
+import pprint
+import json
+import os
+from store.models import DATABASE
+
+def filtering_category(database: dict,
+                       category_key: [int, str],
+                       ordering_key: [None, str] = None,
+                       reverse: bool = False):
+    """
+    Функция фильтрации данных по параметрам
+
+    :param database: База данных.
+    :param category_key: [Опционально] Ключ для группировки категории. Если нет ключа, то рассматриваются все товары.
+    :param ordering_key: [Опционально] Ключ по которому будет произведена сортировка результата.
+    :param reverse: [Опционально] Выбор направления сортировки:
+        False - сортировка по возрастанию;
+        True - сортировка по убыванию.
+    :return: list[dict] список товаров с их характеристиками, попавших под условия фильтрации. Если нет таких элементов,
+    то возвращается пустой список
+    """
+    if category_key is not None:
+        result = [product for product in database.values() if product['category'] == category_key] # фильтр товаров по категории.
+    else:
+        result = [product for product in database.values()]  # Трансформируйте database в список словарей
+    if ordering_key is not None:
+        result.sort(key=lambda x: x[ordering_key], reverse=reverse) # сортировка result по ordering_key и параметру reverse
+    return result
+
+
+# Проверка фильтра def filtering_category
+# if __name__ == "__main__":
+#     from store.models import DATABASE
+#
+#     test = [
+#         {'name': 'Клубника', 'discount': None, 'price_before': 500.0,
+#          'price_after': 500.0,
+#          'description': 'Сладкая и ароматная клубника, полная витаминов, чтобы сделать ваш день ярче.',
+#          'rating': 5.0, 'review': 200, 'sold_value': 700,
+#          'weight_in_stock': 400,
+#          'category': 'Фрукты', 'id': 2, 'url': 'store/images/product-2.jpg',
+#          'html': 'strawberry'},
+#
+#         {'name': 'Яблоки', 'discount': None, 'price_before': 130.0,
+#          'price_after': 130.0,
+#          'description': 'Сочные и сладкие яблоки - идеальная закуска для здорового перекуса.',
+#          'rating': 4.7, 'review': 30, 'sold_value': 70, 'weight_in_stock': 200,
+#          'category': 'Фрукты', 'id': 10, 'url': 'store/images/product-10.jpg',
+#          'html': 'apple'}
+#     ]
+#
+#     pprint.pprint(filtering_category(DATABASE, 'Фрукты', 'price_after', True))  # True
+
+def view_in_cart() -> dict:
+    """
+    Просматривает содержимое cart.json
+
+    :return: Содержимое 'cart.json'
+    """
+    if os.path.exists('cart.json'):  # Если файл существует
+        with open('cart.json', encoding='utf-8') as f:
+            return json.load(f)
+
+    cart = {'products': {}}  # Создаём пустую корзину
+    with open('cart.json', mode='x', encoding='utf-8') as f:   # Создаём файл и записываем туда пустую корзину
+        json.dump(cart, f)
+
+    return cart
+
+
+def add_to_cart(id_product: str) -> bool:
+    """
+    Добавляет продукт в корзину. Если в корзине нет данного продукта, то добавляет его с количеством равное 1.
+    Если в корзине есть такой продукт, то добавляет количеству данного продукта + 1.
+
+    :param id_product: Идентификационный номер продукта в виде строки.
+    :return: Возвращает True в случае успешного добавления, а False в случае неуспешного добавления(товара по id_product
+    не существует).
+    """
+
+    cart = view_in_cart() # TODO Помните, что у вас есть уже реализация просмотра корзины,
+
+    # поэтому, чтобы загрузить данные из корзины, не нужно заново писать код.
+    # TODO Проверьте, а существует ли такой товар в корзине, если нет, то перед тем как его добавить - проверьте есть ли такой
+    # id товара в вашей базе данных DATABASE, чтобы уберечь себя от добавления несуществующего товара.
+    if id_product in [product for product in DATABASE.keys()]:
+        if id_product in [prod for prod in cart["products"].keys()]:
+        # TODO Если товар существует, то увеличиваем его количество на 1
+            count_prod = cart['products'][id_product]
+            count_prod += 1
+            cart['products'][id_product] = count_prod
+        # TODO Не забываем записать обновленные данные cart в 'cart.json'
+        else:
+            cart['products'][id_product] = 1
+        with open('cart.json', 'w', encoding='utf-8') as f:
+                json.dump(cart, f)
+        return True
+    else:
+        return False
+
+
+def remove_from_cart(id_product: str) -> bool:
+    """
+    Добавляет позицию продукта из корзины. Если в корзине есть такой продукт, то удаляется ключ в словаре
+    с этим продуктом.
+
+    :param id_product: Идентификационный номер продукта в виде строки.
+    :return: Возвращает True в случае успешного удаления, а False в случае неуспешного удаления(товара по id_product
+    не существует).
+    """
+    cart = view_in_cart()  # TODO Помните, что у вас есть уже реализация просмотра корзины,
+    # поэтому, чтобы загрузить данные из корзины, не нужно заново писать код.
+    # TODO Проверьте, а существует ли такой товар в корзине, если нет, то возвращаем False.
+    if id_product not in [prod for prod in cart["products"].keys()]:
+        return False
+    # TODO Если существует товар, то удаляем ключ 'id_product' у cart['products'].
+    else:
+        cart["products"].pop(id_product)
+    # TODO Не забываем записать обновленные данные cart в 'cart.json'
+        with open('cart.json', 'w', encoding='utf-8') as f:
+            json.dump(cart, f)
+        return True
+
+
+if __name__ == "__main__":
+    # Проверка работоспособности функций view_in_cart, add_to_cart, remove_from_cart
+    # Для совпадения выходных значений перед запуском скрипта удаляйте появляющийся файл 'cart.json' в папке
+    from store.models import DATABASE
+    print(view_in_cart())  # {'products': {}}
+    print(add_to_cart('1'))  # True
+    print(add_to_cart('0'))  # False
+    print(add_to_cart('1'))  # True
+    print(add_to_cart('2'))  # True
+    print(view_in_cart())  # {'products': {'1': 2, '2': 1}}
+    print(remove_from_cart('0'))  # False
+    print(remove_from_cart('1'))  # True
+    print(view_in_cart())  # {'products': {'2': 1}}
--- store/views.py
+++ store/views.py
@@ -1,13 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.http import JsonResponse
 from .models import DATABASE
+from logic.services import filtering_category, view_in_cart, add_to_cart, remove_from_cart
 
 
 def products_view(request):
     if request.method == "GET":
-        return JsonResponse(DATABASE, json_dumps_params={'ensure_ascii': False,
-                                                 'indent': 4})
+        id = request.GET.get('id')
+        list_prod = [product for product in DATABASE.keys()]
+        if id:
+            if id in list_prod:
+                return JsonResponse(DATABASE[id], json_dumps_params={'ensure_ascii': False,
+                                                                       'indent': 4})
+            elif id not in list_prod:
+                return HttpResponseNotFound("Данного продукта нет в базе данных")
+
+            # Обработка фильтрации из параметров запроса
+        category_key = request.GET.get("category")  # Считали 'category'
+        if ordering_key := request.GET.get("ordering"):  # Если в параметрах есть 'ordering'
+            if request.GET.get("reverse") in ('true', 'True'):  # Если в параметрах есть 'ordering' и 'reverse'=True
+                data = filtering_category(DATABASE, category_key, ordering_key, reverse=True)
+                # TODO Провести фильтрацию с параметрами
+            else:
+                data = filtering_category(DATABASE, category_key, ordering_key, reverse=False)
+                # TODO Провести фильтрацию с параметрами
+        else:
+            data = filtering_category(DATABASE, category_key)
+            # TODO Провести фильтрацию с параметрами
+        # В этот раз добавляем параметр safe=False, для корректного отображения списка в JSON
+        return JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False,
+                                                                 'indent': 4})
 
 
 def shop_view(request):
@@ -16,3 +39,52 @@ def shop_view(request):
             data = f.read()  # Читаем HTML файл
         return HttpResponse(data)  # Отправляем HTML файл как ответ
 
+
+def products_page_view(request, page):
+    if request.method == "GET":
+        if isinstance(page, str):
+            for data in DATABASE.values():
+                if data['html'] == page:
+                    with open(f'store/products/{page}.html', encoding="utf-8") as f:
+                        page_product = f.read()
+                    return HttpResponse(page_product)
+
+        elif isinstance(page, int):
+            data = DATABASE.get(str(page))
+            if data:
+                with open(f'store/products/{data["html"]}.html', encoding="utf-8") as f:
+                    page_product = f.read()
+                return HttpResponse(page_product)
+
+        return HttpResponse(status=404)
+
+
+def cart_view(request):
+    if request.method == "GET":
+        data = view_in_cart()  # TODO Вызвать ответственную за это действие функцию
+        return JsonResponse(data, json_dumps_params={'ensure_ascii': False,
+                                                     'indent': 4})
+
+
+def cart_add_view(request, id_product):
+    if request.method == "GET":
+        result = add_to_cart(id_product) # TODO Вызвать ответственную за это действие функцию
+        if result:
+            return JsonResponse({"answer": "Продукт успешно добавлен в корзину"},
+                                json_dumps_params={'ensure_ascii': False})
+
+        return JsonResponse({"answer": "Неудачное добавление в корзину"},
+                            status=404,
+                            json_dumps_params={'ensure_ascii': False})
+
+
+def cart_del_view(request, id_product):
+    if request.method == "GET":
+        result = remove_from_cart(id_product) # TODO Вызвать ответственную за это действие функцию
+        if result:
+            return JsonResponse({"answer": "Продукт успешно удалён из корзины"},
+                                json_dumps_params={'ensure_ascii': False})
+
+        return JsonResponse({"answer": "Неудачное удаление из корзины"},
+                            status=404,
+                            json_dumps_params={'ensure_ascii': False})
