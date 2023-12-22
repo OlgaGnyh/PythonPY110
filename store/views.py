bc490d6bcdd6789730e9f797e5ecc6c96a60db0a
OlgaGnyh <gnyh@mail.ru>
2023-12-22 21:16:18 +0300

    сделана лабораторная 4

--- store/views.py
+++ store/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.http import JsonResponse
 from .models import DATABASE
 from logic.services import filtering_category, view_in_cart, add_to_cart, remove_from_cart
+from django.shortcuts import render
 
 
 def products_view(request):
@@ -35,10 +36,23 @@ def products_view(request):
 
 def shop_view(request):
     if request.method == "GET":
-        with open('store/shop.html', encoding="utf-8") as f:
-            data = f.read()  # Читаем HTML файл
-        return HttpResponse(data)  # Отправляем HTML файл как ответ
-
+        # with open('store/shop.html', encoding="utf-8") as f:
+        #     data = f.read()  # Читаем HTML файл
+        # return HttpResponse(data)  # Отправляем HTML файл как ответ
+        # return render(request,
+        #               'store/shop.html',
+        #               context={"products": DATABASE.values()})
+        if ordering_key := request.GET.get("ordering"):
+            if request.GET.get("reverse") in ('true', 'True'):
+                data = filtering_category(DATABASE, category_key, ordering_key,
+                                          True)
+            else:
+                data = filtering_category(DATABASE, category_key, ordering_key)
+        else:
+            data = filtering_category(DATABASE, category_key)
+        return render(request, 'store/shop.html',
+                      context={"products": data,
+                               "category": category_key})
 
 def products_page_view(request, page):
     if request.method == "GET":
@@ -61,9 +75,25 @@ def products_page_view(request, page):
 
 def cart_view(request):
     if request.method == "GET":
-        data = view_in_cart()  # TODO Вызвать ответственную за это действие функцию
-        return JsonResponse(data, json_dumps_params={'ensure_ascii': False,
-                                                     'indent': 4})
+        data = view_in_cart()  # отображает корзину
+        if request.GET.get('format') == 'JSON':
+            return JsonResponse(data, json_dumps_params={'ensure_ascii': False,
+                                                         'indent': 4})
+        products = []  # Список продуктов
+        for product_id, quantity in data['products'].items():
+            product = DATABASE[product_id]  # 1. Получите информацию о продукте из DATABASE по его product_id. product будет словарём
+            # 2. в словарь product под ключом "quantity" запишите текущее значение товара в корзине
+            cart = view_in_cart()
+            product["quantity"] = cart['products'][product_id]
+            quantity = product["quantity"]
+            product[
+                "price_total"] = f"{quantity * product['price_after']:.2f}"  # добавление общей цены позиции с ограничением в 2 знака
+            # 3. добавьте product в список products
+            products.append(product)
+
+        return render(request, "store/cart.html", context={"products": products})
+        # return JsonResponse(data, json_dumps_params={'ensure_ascii': False,
+        #                                              'indent': 4})
 
 
 def cart_add_view(request, id_product):
