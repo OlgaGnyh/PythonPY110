e671d0e8309f2f39da34fd53a97168f83e8fc9e7
Olga <gnyh@mail.ru>
2024-01-04 13:12:27 +0300

    сделана 6 лаба

--- logic/services.py
+++ logic/services.py
@@ -154,3 +154,81 @@ if __name__ == "__main__":
     print(remove_from_cart('0'))  # False
     print(remove_from_cart('1'))  # True
     print(view_in_cart())  # {'products': {'2': 1}}
+
+def add_user_to_wishlist(request, username: str) -> None:
+    """
+    Добавляет пользователя в базу данных корзины, если его там не было.
+
+    :param username: Имя пользователя
+    :return: None
+    """
+    wishlist_users = view_in_wishlist(request)  # Чтение всей базы корзин
+    wishlist = wishlist_users.get(username)  # Получение корзины конкретного пользователя
+
+    if not wishlist:  # Если пользователя до настоящего момента не было в корзине, то создаём его и записываем в базу
+        with open('wishlist.json', mode='w', encoding='utf-8') as f:
+            wishlist_users[username] = {'products': []}
+            json.dump(wishlist_users, f)
+
+def view_in_wishlist(request) -> dict:
+    """
+    Просматривает содержимое wishlist.json
+
+    :return: Содержимое 'wishlist.json'
+    """
+    if os.path.exists('wishlist.json'):  # Если файл существует
+        with open('wishlist.json', encoding='utf-8') as f:
+            return json.load(f)
+
+    user = get_user(request).username
+    wishlist = {user: {'products': []}}  # Создаём пустую корзину
+    with open('wishlist.json', mode='x', encoding='utf-8') as f:   # Создаём файл и записываем туда пустую корзину
+        json.dump(wishlist, f)
+
+    return wishlist
+
+
+def add_to_wishlist(request, id_product: str) -> bool:
+    """
+    Добавляет продукт в избранное. Если нет данного продукта, то добавляет его id.
+    :param id_product: Идентификационный номер продукта в виде строки.
+    :return: Возвращает True в случае успешного добавления, а False в случае неуспешного добавления(товара по id_product
+    не существует).
+    """
+    wishlist_users = view_in_wishlist(request)
+    wishlist = wishlist_users[get_user(request).username]
+
+    # существует ли такой товар в избранном, если нет, то перед тем как его добавить - проверьте есть ли такой
+    # id товара в вашей базе данных DATABASE, чтобы уберечь себя от добавления несуществующего товара.
+    if id_product in [product for product in DATABASE.keys()]:
+        if id_product not in [prod for prod in wishlist["products"]]:
+
+            wishlist['products'].append(id_product)
+        with open('wishlist.json', 'w', encoding='utf-8') as f:
+                json.dump(wishlist_users, f)
+        return True
+    else:
+        return False
+
+
+def remove_from_wishlist(request, id_product: str) -> bool:
+    """
+    Удаляет позицию продукта из избранного. Если в избранном есть такой продукт, то удаляется ключ в словаре
+    с этим продуктом.
+
+    :param id_product: Идентификационный номер продукта в виде строки.
+    :return: Возвращает True в случае успешного удаления, а False в случае неуспешного удаления(товара по id_product
+    не существует).
+    """
+    wishlist_users = view_in_wishlist(request)
+    wishlist = wishlist_users[get_user(request).username]
+    # существует ли такой товар в корзине, если нет, то возвращаем False.
+    if id_product not in [prod for prod in wishlist["products"]]:
+        return False
+    # Если существует товар, то удаляем ключ 'id_product' у wishlist['products'].
+    else:
+        wishlist["products"].remove(id_product)
+    # записать обновленные данные cart в 'cart.json'
+        with open('wishlist.json', 'w', encoding='utf-8') as f:
+            json.dump(wishlist_users, f)
+        return True
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,8 +1,85 @@
-
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from store.models import DATABASE
+from logic.services import view_in_wishlist, add_to_wishlist, remove_from_wishlist
+from django.contrib.auth import get_user
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseNotFound
 
 
+@login_required(login_url='login:login_view')
 def wishlist_view(request):
     if request.method == "GET":
-        return render(request, 'wishlist/wishlist.html')
-        # прописать отображение избранного. Путь до HTML - wishlist/wishlist.html
+        current_user = get_user(request).username
+        data = view_in_wishlist(request)[current_user]
+        # получить продукты из избранного для пользователя
+        products = []
+        # список словарей продуктов с их характеристиками
+        for product_id in data['products']:
+            product = DATABASE[product_id]
+
+            products.append(product)
+        return render(request, 'wishlist/wishlist.html',
+                      # context={"products": DATABASE.values()},
+                      context={"products": products})
+        # отображение избранного. Путь до HTML - wishlist/wishlist.html
+
+
+@login_required(login_url='login:login_view')
+def wishlist_add_json(request, id_product: str):
+    """
+    Добавление продукта в избранное и возвращение информации об успехе или неудаче в JSON
+    """
+    if request.method == "GET":
+        result = add_to_wishlist(request, id_product)  # обработчик из services.py добавляющий продукт в избранное
+        if result:
+            return JsonResponse({"answer": "Продукт успешно добавлен в избранное"},
+                                json_dumps_params={'ensure_ascii': False})
+            # JsonResponse с ключом "answer" и значением "Продукт успешно добавлен в избранное"
+
+        return JsonResponse({"answer": "Неудачное добавление в избранное"},
+                            status=404,
+                            json_dumps_params={'ensure_ascii': False})
+        # JsonResponse с ключом "answer" и значением "Неудачное добавление в избранное" и параметром status=404
+
+
+def wishlist_del_json(request, id_product: str):
+    """
+    Удаление продукта из избранного и возвращение информации об успехе или неудаче в JSON
+    """
+    if request.method == "GET":
+        result = remove_from_wishlist(request, id_product)
+        # обработчик из services.py удаляющий продукт из избранного
+        if result:
+            return JsonResponse({"answer": "Продукт успешно удалён из избранного"},
+                                json_dumps_params={'ensure_ascii': False})
+            # JsonResponse с ключом "answer" и значением "Продукт успешно удалён из избранного"
+        return JsonResponse({"answer": "Неудачное удаление из избранного"},
+                            status=404,
+                            json_dumps_params={'ensure_ascii': False})
+        # JsonResponse с ключом "answer" и значением "Неудачное удаление из избранного" и параметром status=404
+
+
+def wishlist_json(request):
+    """
+    Просмотр всех продуктов в избранном для пользователя и возвращение этого в JSON
+    """
+    if request.method == "GET":
+        current_user = get_user(request).username  # from django.contrib.auth import get_user
+        data = view_in_wishlist(request)[current_user]
+        # данные о списке товаров в избранном у пользователя
+        if data:
+            return JsonResponse(data)  # JsonResponse c data
+        return JsonResponse({"answer": "Пользователь не авторизирован"},
+                            status=404,
+                            json_dumps_params={'ensure_ascii': False})
+        # JsonResponse с ключом "answer" и значением "Пользователь не авторизирован" и параметром status=404
+
+
+def wishlist_remove_view(request, id_product):
+    if request.method == "GET":
+        result = remove_from_wishlist(request, id_product)
+        if result:
+            return redirect("wishlist:wishlist")
+
+        return HttpResponseNotFound("Неудачное удаление из избранного")
